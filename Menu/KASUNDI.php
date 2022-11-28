25db713ac885bbaf19a64b03290322835b891b7d
Mahmudul Hoque <67953269+HoqueMahmudul@users.noreply.github.com>
2022-11-28 17:53:18 +0600

    Add files via upload

---
+++ Menu/KASUNDI.php
@@ -0,0 +1,61 @@
+<table class="table text-light">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Quantity</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Parata</td>
+      <td>15</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Dal</td>
+      <td>20</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Bahji</td>
+      <td>20</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Khichuri</td>
+      <td>60</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Chicken Curry</td>
+      <td>50</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Fried Rice</td>
+      <td>70</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Chicken Chili Onion</td>
+      <td>90</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Cocacola </td>
+      <td>20</td>
+      <td>0 <button class='btn btn-success'>+</button> <button class='btn btn-danger'>-</button></td>
+    </tr>
+    
+  </tbody>
+</table>
